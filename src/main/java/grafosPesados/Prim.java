aaca2de7a96eaddb184bab075e6545a543e6c58d
domeki <domeki151190@gmail.com>
2021-08-04 14:51:26 -0400

    prim

--- src/main/java/grafosPesados/Prim.java
+++ src/main/java/grafosPesados/Prim.java
@@ -5,10 +5,59 @@
  */
 package grafosPesados;
 
+
+import java.util.List;
+
 /**
  *
  * @author user
  */
 public class Prim {
+    private GrafoPesado grafoAux;
+    private GrafoPesado grafoOriginal;
+    private UtilsRecorrido controlMarcado;
+
+    public Prim(GrafoPesado grafoOriginal){
+        this.grafoOriginal = grafoOriginal;
+        this.grafoAux = new GrafoPesado(this.grafoOriginal.cantidadDeVertices());
+        this.controlMarcado = new UtilsRecorrido(this.grafoAux.cantidadDeVertices()); 
+        this.insertarVertices();
+    }
+    public void insertarVertices(){  
+        int verticeOrigen = 0;
+        int verticeDestino = 0;
+        double menorPeso = 999999;
+        this.controlMarcado.marcarVertice(0);
+        do{
+            for (int i = 0; i < this.grafoAux.cantidadDeVertices(); i++) {
+                if (this.controlMarcado.estaMarcado(i)) {
+                        List<AdyacenteConPeso> adyacenteVerticeEnTurno = this.grafoOriginal.listaDeAdyacencia.get(i);
+                    for (AdyacenteConPeso ady: adyacenteVerticeEnTurno) {
+                        if (ady.getPeso() < menorPeso) {
+                                if (!(this.controlMarcado.estaMarcado(ady.getIndiceVertice()))) {
+                                    menorPeso = ady.getPeso();
+                                    verticeOrigen = i;
+                                    verticeDestino = ady.getIndiceVertice();
+                                }
+                        }
+                    }
+                }
+            }
+        this.grafoAux.insertarArista(verticeOrigen, verticeDestino, menorPeso);
+        this.controlMarcado.marcarVertice(verticeOrigen);
+        this.controlMarcado.marcarVertice(verticeDestino);
+        menorPeso = 999999;
+        }while(!this.controlMarcado.estanTodosMarcados());            
+    }
+
+    @Override
+    public String toString() {
+        String s= "Algoritmo de Prim \n";
+        for (int i = 0; i < this.grafoAux.cantidadDeVertices(); i++) {
+            s = s +"|"+ i + "|-> " + this.grafoAux.listaDeAdyacencia.get(i)  + "\n";
+            //adyacentesDeVertice(i)
+        }
+        return s;
+    }
     
 }
